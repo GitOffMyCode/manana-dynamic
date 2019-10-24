fe067d1cd03c93a7e50309c664c42675cf51b8ae
Genevieve Heald <gennaheald@outlook.com>
2019-10-24 15:00:59 +0100

    input box function changed, minor changes to style

--- main.js
+++ main.js
@@ -6,23 +6,25 @@ doItList.addEventListener('click', deleteItem);
 
 function addIt (event) {
     event.preventDefault();
-    const taskValue = document.getElementById('task').value;
-    const li = document.createElement('li');
-    li.className = "mx-3 taskText";
-    li.appendChild(document.createTextNode(taskValue))
-    const deleteBtn = document.createElement('button');
-    deleteBtn.className = "fa fa-trash icon-bin delete";
-    deleteBtn.style = "float: right;"
-    const hr = document.createElement('hr');
-    hr.className = "taskDivider";
-    li.append(deleteBtn);
-    doItList.append(li);
-    doItList.append(hr);
+    const taskValue = document.getElementById('add-task').value;
+    if (taskValue.length >= 1) {
+        const li = document.createElement('li');
+        li.className = "mx-3 taskText";
+        li.appendChild(document.createTextNode(taskValue))
+        const deleteBtn = document.createElement('button');
+        deleteBtn.className = "fa fa-trash icon-bin";
+        deleteBtn.style = "float: right;";
+        const hr = document.createElement('hr');
+        hr.className = "taskDivider";
+        li.append(deleteBtn);
+        doItList.append(li);
+        doItList.append(hr);
+    }
 }
 
 
 function deleteItem(event) {
-    if(event.target.classList.contains('delete')) {
+    if(event.target.classList.contains('icon-bin')) {
         const li = event.target.parentElement;
         const hr = event.target.parentElement.nextElementSibling;
         doItList.removeChild(li);
