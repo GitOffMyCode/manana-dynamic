fa433ae212a41479ec6a294daf15a45d5547096d
Genevieve Heald <gennaheald@outlook.com>
2019-10-24 14:05:28 +0100

    add and delete items completed

--- main.js
+++ main.js
@@ -1,56 +1,31 @@
-// access the ADD IT form by creating a variable
 const form = document.getElementById('addTask');
-// access DO IT list of items by creating a variable
 const doItList = document.getElementById('doItItem');
 
-// ADD IT EVENT
 form.addEventListener('submit', addIt); 
-
-// DELETE EVENT
 doItList.addEventListener('click', deleteItem);
 
-function addIt (e) {
-    // prevent default form submission
-    e.preventDefault();
-    // get input value saved to a variable
+function addIt (event) {
+    event.preventDefault();
     const taskValue = document.getElementById('task').value;
-
-    // create new li
     const li = document.createElement('li');
-    // add a class
     li.className = "mx-3 taskText";
-    // add new value to new li element
     li.appendChild(document.createTextNode(taskValue))
-
-    // create new span
-    const span = document.createElement('span');
-    // add style
-    span.style = "float: right;";
-
-    // create new deleteBtn
     const deleteBtn = document.createElement('button');
-    // add classes to del button
     deleteBtn.className = "fa fa-trash icon-bin delete";
-
-    // create new hr
+    deleteBtn.style = "float: right;"
     const hr = document.createElement('hr');
     hr.className = "taskDivider";
-
-    // append button to span
-    span.appendChild(deleteBtn);
-    // append span to li
-    li.appendChild(span);
-    // append li to doItList
-    doItList.appendChild(li);
-    // add hr to doItList
-    doItList.appendChild(hr);
+    li.append(deleteBtn);
+    doItList.append(li);
+    doItList.append(hr);
 }
 
 
-function deleteItem(e) {
-    if(e.target.classList.contains('delete')) {
-            // this bit not working right!!!
-            const li = e.target.parentElement;
-            doItList.removeChild(li);
+function deleteItem(event) {
+    if(event.target.classList.contains('delete')) {
+        const li = event.target.parentElement;
+        const hr = event.target.parentElement.nextElementSibling;
+        doItList.removeChild(li);
+        doItList.removeChild(hr);
     }
 }
